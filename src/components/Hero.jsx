ce84a11efa279efab961cd04e66700975f590cf4
Dexter Whelan <whelandexter@gmail.com>
2025-05-11 22:15:30 +0100

    hero done, parallax done, floating banners notifications, rings designed in background, other logo section to show other companies'section on a site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,9 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -30,7 +33,7 @@ const Hero = () => {
         text-center mb-[4rem] md:mb-20 lg:mb:[6rem]"
         >
           <h1 className="h1 mb-6">
-            Explore the possibilities of AI chatting with{" "}
+            Explore the Possibilities of &nbsp;AI&nbsp;chatting with{" "}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -64,6 +67,7 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
@@ -73,6 +77,12 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
                 </ScrollParallax>
               </div>
             </div>
@@ -94,7 +104,10 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
+
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
+      <BottomLine />
     </Section>
   );
 };
